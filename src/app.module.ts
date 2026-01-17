eedc69f86fcabbb80efbf9dc70c2923b68ae97f7
llslucas <lucas.souza473@outlook.com>
2026-01-16 23:44:34 -0300

    Add `Production` module to NestJS Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { ProductionModule } from './production/production.module';
 
 @Module({
-  imports: [],
+  imports: [ProductionModule],
   controllers: [],
   providers: [],
 })
