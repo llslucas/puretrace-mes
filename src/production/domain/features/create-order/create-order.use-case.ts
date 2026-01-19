b527fece30169fa6beeaefd29d811f20ac5ea960
llslucas <lucas.souza473@outlook.com>
2026-01-19 15:41:41 -0300

    feat: add effect schema validation on `create order use case` for double validation

--- src/production/domain/features/create-order/create-order.use-case.ts
+++ src/production/domain/features/create-order/create-order.use-case.ts
@@ -1,4 +1,4 @@
-import { Effect } from 'effect';
+import { Effect, Schema } from 'effect';
 import { ProductionOrderModel } from '../../entities/production-order.model';
 import { ProductionOrderRepositoryTag } from '../../entities/production-order.repository';
 import { CreateProductionOrderDto } from './create-order.dto';
@@ -6,8 +6,12 @@ import { CreateProductionOrderDto } from './create-order.dto';
 export class CreateOrderUseCase {
   execute(input: CreateProductionOrderDto) {
     return Effect.gen(function* (_) {
+      const dto = yield* _(
+        Schema.decodeUnknown(CreateProductionOrderDto)(input),
+      );
+
       const repository = yield* _(ProductionOrderRepositoryTag);
-      const orderDomainEntity = yield* _(ProductionOrderModel.create(input));
+      const orderDomainEntity = yield* _(ProductionOrderModel.create(dto));
 
       const savedOrder = yield* _(repository.save(orderDomainEntity));
 
