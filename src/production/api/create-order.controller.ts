135a6c284ff46e9d27c41e0f8faff3eec70f0d4e
llslucas <lucas.souza473@outlook.com>
2026-01-31 17:55:07 -0300

    refactor: improve error handling on `create-order.controller`

--- src/production/api/create-order.controller.ts
+++ src/production/api/create-order.controller.ts
@@ -5,14 +5,14 @@ import {
   Post,
   UsePipes,
 } from '@nestjs/common';
-import { Effect, Either } from 'effect';
+import { Effect } from 'effect';
 import { EffectValidationPipe } from 'src/shared/pipes/effect-validation.pipe';
 import {
   ProductionOrderRepository,
   ProductionOrderRepositoryTag,
 } from '../domain/entities/production-order.repository';
-import { CreateOrderUseCase } from '../domain/features/create-order/create-order.use-case';
 import { CreateProductionOrderDto } from '../domain/features/create-order/create-order.dto';
+import { CreateOrderUseCase } from '../domain/features/create-order/create-order.use-case';
 
 @Controller('production')
 export class ProductionController {
@@ -22,19 +22,11 @@ export class ProductionController {
   @UsePipes(new EffectValidationPipe())
   async handle(@Body() body: CreateProductionOrderDto) {
     const useCase = new CreateOrderUseCase();
-    const program = useCase.execute(body);
-    const safeProgram = Effect.either(program);
-
-    const runnable = safeProgram.pipe(
+    const program = useCase.execute(body).pipe(
       Effect.provideService(ProductionOrderRepositoryTag, this.repository),
+      Effect.mapError((e) => new BadRequestException(e)),
     );
 
-    const result = await Effect.runPromise(runnable);
-
-    if (Either.isRight(result)) {
-      return result.right;
-    } else {
-      throw new BadRequestException(result.left);
-    }
+    return await Effect.runPromise(program);
   }
 }
